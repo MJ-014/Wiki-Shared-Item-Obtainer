524acd7df81111ffe7a457a239f8ca14c33c0064
Navid <51940962+Navid-d@users.noreply.github.com>
2022-01-06 23:56:14 +0330

    Add files via upload

---
+++ multi_cat.js
@@ -0,0 +1,89 @@
+//categories = ['Citizens of Beach City', 'Fusion Gems']
+
+async function getJason(url, arr){
+    var response = await fetch(url);
+    var jason = await response.json();
+    if(jason["continue"] !== undefined){
+        var cont = await jason['continue']['cmcontinue']
+        try{
+            jason['query']['categorymembers'].forEach(element => {
+                arr.push(element['title']);
+            })
+        }
+        catch(error){
+            document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
+        }
+        //The rest of pages of results
+        while(true){
+            var response = await fetch(url + "&cmcontinue=" + cont, {method: 'GET'});
+            var jason = await response.json();
+            if(jason["continue"] !== undefined){
+                var cont = jason['continue']['cmcontinue']
+            }
+            else{
+                try{
+                    jason['query']['categorymembers'].forEach(element => {
+                        arr.push(element['title']);
+                    })
+                }
+                catch(error){
+                    document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
+                }
+                break;
+            }
+            try{
+                jason['query']['categorymembers'].forEach(element => {
+                    arr.push(element['title']);
+                })
+            }
+            catch(error){
+                document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
+            }
+            
+        }
+    }
+    else{
+        try{
+            jason['query']['categorymembers'].forEach(element => {
+                arr.push(element['title']);
+            })
+        }
+        catch(error){
+            document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
+        }
+    }
+
+}
+
+function intersection(first, second){
+    var s = new Set(second);
+    return first.filter(item => s.has(item));
+};
+
+async function main(){
+    var wiki = document.getElementById("wiki").value;
+    if(wiki == ""){
+        alert("Please provide a wiki.");
+        return;
+    }
+    var catName = document.getElementById("cat").value.split("\n");
+    var rips1 = [];
+    var rips2 = [];
+    
+    document.getElementById("output").innerHTML = "Working ion your request...";
+    
+    for (let item of catName){
+        if(catName.indexOf(item) == 0){
+            await getJason("https://" + wiki + ".fandom.com/api.php?action=query&cmtitle=Category:" + item + "&list=categorymembers&cmlimit=5&origin=*&format=json", rips1); 
+        }
+        else{
+            await getJason("https://" + wiki + ".fandom.com/api.php?action=query&cmtitle=Category:" + item + "&list=categorymembers&cmlimit=5&origin=*&format=json", rips2); 
+            rips1 = await intersection(rips1, rips2);
+            rips2 = [];
+        }
+    }
+    
+    document.getElementById("output").innerHTML = rips1.join("<br>");
+}
+
+
