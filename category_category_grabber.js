e44f5293e59312f806c645177979d73ba8f96bbb
MJ <51940962+MJ-014@users.noreply.github.com>
2024-02-20 16:14:52 +0330

    Update category_category_grabber.js

--- category_category_grabber.js
+++ category_category_grabber.js
@@ -1,158 +1,102 @@
-async function getJason(url, arr) {
-    var response = await fetch(url);
-    var jason = await response.json();
-    if (jason.continue !== undefined) {
-        var cat = await jason.continue.cmcontinue
-        try {
-            jason.query.categorymembers.forEach(element => {
-                arr.push(element.title);
-            })
+<html>
+
+<head>
+    <title>SiIvaGunner Wiki Category Category Grabber</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    <style>
+        body {
+            background-color: #eee;
         }
-        catch (error) {
-            document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
+
+        .header-btn {
+            border-radius: 0;
+            margin: 0;
+            padding: 1px;
+            padding-right: 15px;
         }
-        while (true) {
-            var response = await fetch(url + "&cmcontinue=" + cat, { method: 'GET' });
-            var jason = await response.json();
-            if (jason["continue"] !== undefined) {
-                var cat = jason.continue.cmcontinue
-            }
-            else {
-                try {
-                    jason.query.categorymembers.forEach(element => {
-                        arr.push(element.title);
-                    })
-                }
-                catch (error) {
-                    document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
-                }
-                break;
-            }
-            try {
-                jason.query.categorymembers.forEach(element => {
-                    arr.push(element.title);
-                })
-            }
-            catch (error) {
-                document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
-            }
 
+        .header-btn span {
+            font-size: small;
+            border-radius: 0;
         }
-    }
-    else {
-        try {
-            jason.query.categorymembers.forEach(element => {
-                arr.push(element.title);
-            })
+
+        /* Local Styling */
+        input {
+            width: 70% !important;
         }
-        catch (error) {
-            document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
+
+        #size {
+            width: min-content !important;
         }
-    }
+    </style>
 
-}
+    <script src="category_category_grabber.js"></script>
+</head>
 
-async function getCategories(url) {
-    var arr = [];
-    var response = await fetch("https://siivagunner.fandom.com/api.php?action=query&prop=categories&titles=" + encodeURIComponent(url) + "&format=json&origin=*&cllimit=500");
-    var jason = await response.json();
-    try {
-        jason.query.pages[Object.keys(jason.query.pages)[0]].categories.forEach(element => {
-            arr.push(element.title);
-        });
-    }
-    catch (error) {
-        document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
-    }
+<body>
+    <div class="m-2 mb-4">
+        <div class="m-2 mb-4">
+            <a href="index.html" class="btn header-btn">
+                <span>Shared Item Grabber</span>
+            </a>
+            <a href="contributor_game.html" class="btn header-btn">
+                <span>Category Category Grabber</span>
+            </a>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12 col-md-4 pb-1  text-end pe-5">Category:</div>
+            <div class=" col-12 col-md-6 pb-1">
+                <input id="cat" class="form-control">
+            </div>
+        </div>
+    </div>
+    <div class="container" id="extra">
+        <div class="row text-center">
+            <div class="col-12 col-md-4 pb-1  text-end pe-5">Filter Category:</div>
+            <div class=" col-12 col-md-6 pb-1">
+                <input id="fil" class="form-control">
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-12 col-md-4 pb-1  text-end pe-5">Negative Category:</div>
+            <div class=" col-12 col-md-6 pb-1">
+                <input id="neg" class="form-control">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row text-center mb-5">
+            <div class="col-12 col-md-4 text-end pe-5">
+                Show category size:
+            </div>
+            <div class="col-12 col-md-4 text-start d-flex align-center pt-1">
+                <input type="checkbox" id="size" name="size" onchange="checkboxCheck()">
+            </div>
+        </div>
+        <div class="row text-center pb-3">
+            <button onclick="main()" class="col-12 btn btn-secondary"
+                style="width: fit-content; margin: 0 auto;">Search</button>
+        </div>
+        <div class="row">
+            <div class="col-md-4 d-none d-md-block"></div>
+            <p id="output" class="col-12 col-md-8" style="width: fit-content;tab-size: 4;">
+                Enter a category in the "Category" field.<br>This tool will go through the pages in your inputted
+                category, and count all of their categories<br>So you can see how many categories are in the pages of
+                your desired category!<br><br>The Filter category allows you to filter the result by their own
+                category<br>For example, you can enter the following into that field to get your desired results!<br>
+                <b>&emsp;&emsp;- Playlists:</b> Will show you only the games<br>
+                <b>&emsp;&emsp;- Albums:</b> Will show you only the albums<br>
+                <b>&emsp;&emsp;- Rips by author:</b> Will show you only the rippers<br>
+                <b>&emsp;&emsp;- Rips by month:</b> Will show you only the months<br>
+                And so on!<br>The Negative category will do the opposite, and will remove the category you don't want to
+                see
+            </p>
+        </div>
+    </div>
+</body>
 
-    return arr;
-}
-
-async function countOccurrences(inputList) {
-    const dictionary = {};
-
-    var x2 = []
-    //x2 = await getCategories(rip);
-    for (let item of inputList) {
-        if (dictionary[item]) {
-            dictionary[item]++;
-        } else {
-            dictionary[item] = 1;
-        }
-    }
-
-    return dictionary;
-}
-
-async function sort_object(obj) {
-    var obj = await countOccurrences(obj);
-    const entries = Object.entries(obj);
-    entries.sort((a, b) => b[1] - a[1]);
-    const sortedDictionary = Object.fromEntries(entries);
-
-    return sortedDictionary;
-}
-
-async function dictionaryToString(obj) {
-    let resultString = '';
-
-    var obj = await sort_object(obj);
-
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            resultString += `${obj[key]} - ${key.replace('‌', '')}<br>`;
-        }
-    }
-
-    return resultString.trim();
-}
-
-async function main() {
-    var cat = document.getElementById("cat").value;
-    var catFilter = "Category:" + document.getElementById("fil").value.split("\n");
-    var catNeg = "Category:" + document.getElementById("neg").value.split("\n");
-
-    var rips1 = [];
-    var rips2 = [];
-    var rips3 = [];
-
-    if (cat == "") {
-        alert("Please provide a category.");
-        return;
-    }
-
-    document.getElementById("output").innerHTML = "Working on your request...";
-
-    await getJason("https://siivagunner.fandom.com/api.php?action=query&cmtitle=Category:" + encodeURIComponent(cat) + "&list=categorymembers&cmlimit=500&origin=*&format=json", rips1);
-
-    document.getElementById("output").innerHTML = "Input category fetched...";
-    
-    for (let item of rips1) {
-        rips2 = await getCategories(item);
-        document.getElementById("output").innerHTML = "Storing inner categories %" + Math.floor((rips1.indexOf(item)/rips1.length)*100) + " [" + rips1.indexOf(item) + "/" + rips1.length + "]" + "...";
-        rips3 = rips3.concat(rips2);
-    }
-
-    document.getElementById("output").innerHTML = "Inner categories fetched...";
-
-
-    if (catFilter != "Category:" || catNeg != "Category:") {
-        tempRips = rips3.filter((item, index) => rips3.indexOf(item) === index)
-        for (let secondCat of tempRips) {
-            document.getElementById("output").innerHTML = "Applying filter %" + Math.floor((tempRips.indexOf(secondCat)/tempRips.length)*100) + " [" + tempRips.indexOf(secondCat) + "/" + tempRips.length + "]" + "...";
-            categoryCategory = await getCategories(secondCat);
-            if (catFilter != "Category:") {
-                if (!categoryCategory.includes(catFilter)) {
-                    rips3 = rips3.filter(item => item !== secondCat);
-                }
-            }
-            if (catNeg != "Category:") {
-                if (categoryCategory.includes(catNeg)) {
-                    rips3 = rips3.filter(item => item !== secondCat);
-                }
-            }
-        }
-    }
-
-    document.getElementById("output").innerHTML = await dictionaryToString(rips3);
-}
+</html>
