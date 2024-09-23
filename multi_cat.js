70e3c1b9ee5ab1a947a9ab08962008726822febd
MJ <51940962+MJ-014@users.noreply.github.com>
2024-09-23 13:25:38 +0330

    Update multi_cat.js
    Added comments and made the code better to read.

--- multi_cat.js
+++ multi_cat.js
@@ -1,8 +1,33 @@
 async function getJason(url, arr) {
-    var response = await fetch(url);
+    // Get category length
+    var countJason = await fetch("https://" + wiki + "/api.php?action=query&prop=categoryinfo&titles=Category:" + url + "&origin=*&format=json")
+    var countJasonJason = await countJason.json();
+    var count = countJasonJason.query.pages[Object.keys(countJasonJason.query.pages)[0]].categoryinfo.size;
+
+    // Getting category items
+    // Getting first set of items
+    var response = await fetch("https://" + wiki + "/api.php?action=query&cmtitle=Category:" + url + "&list=categorymembers&cmlimit=500&origin=*&format=json", { method: 'GET' });
     var jason = await response.json();
-    if (jason.continue !== undefined) {
+
+    try {
+        jason.query.categorymembers.forEach(element => {
+            //document.getElementById("output").innerHTML = "Getting categories %" + Math.floor((arr.indexOf(element.title) / count) * 100) + " [" + arr.indexOf(element.title) + "/" + count + "]" + "...";
+            arr.push(element.title);
+        })
+    }
+    catch (error) {
+        document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
+    }
+
+    // Getting the rest of the items
+    while (jason.continue !== undefined) {
         var cont = await jason.continue.cmcontinue
+        
+        var response = await fetch("https://" + wiki + "/api.php?action=query&cmtitle=Category:" + url + "&list=categorymembers&cmlimit=500&origin=*&format=json&cmcontinue=" + cont, { method: 'GET' });
+        var jason = await response.json();
+        if (jason.continue !== undefined) {
+            var cont = jason.continue.cmcontinue
+        }
         try {
             jason.query.categorymembers.forEach(element => {
                 arr.push(element.title);
@@ -11,49 +36,11 @@ async function getJason(url, arr) {
         catch (error) {
             document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
         }
-        //The rest of pages of results
-        while (true) {
-            var response = await fetch(url + "&cmcontinue=" + cont, { method: 'GET' });
-            var jason = await response.json();
-            if (jason["continue"] !== undefined) {
-                var cont = jason.continue.cmcontinue
-            }
-            else {
-                try {
-                    jason.query.categorymembers.forEach(element => {
-                        arr.push(element.title);
-                    })
-                }
-                catch (error) {
-                    document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
-                }
-                break;
-            }
-            try {
-                jason.query.categorymembers.forEach(element => {
-                    arr.push(element.title);
-                })
-            }
-            catch (error) {
-                document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
-            }
 
-        }
     }
-    else {
-        try {
-            jason.query.categorymembers.forEach(element => {
-                arr.push(element.title);
-            })
-        }
-        catch (error) {
-            document.getElementById("output").innerHTML = "There was a problem. Please check your input.";
-        }
-    }
-
 }
 
-function intersection(first, second) {
+async function intersection(first, second) {
     var s = new Set(second);
     return first.filter(item => s.has(item));
 };
@@ -61,6 +48,7 @@ function intersection(first, second) {
 var wiki
 
 async function main() {
+    // Get wiki
     wiki = document.getElementById("wiki").value;
     if (wiki == "") {
         alert("Please provide a wiki.");
@@ -72,17 +60,17 @@ async function main() {
         wiki = wiki + '/w';
     }
 
+    // Get items
     var catName1 = document.getElementById("cat1").value.split("\n");
     var catName2 = document.getElementById("cat2").value.split("\n");
     var nameFilter = document.getElementById("nameFilter").value.split("\n");
     var catNeg = document.getElementById("neg").value.split("\n");
-    if (catName2 == "") {
-        catName2 = document.getElementById("cat1").value.split("\n");
-    }
     var method;
+
+    // Lists and such
     var rips1 = [];
-    var rips2_1 = [];
     var rips2 = [];
+    var rips2_1 = [];
     var negRips = [];
 
     if (document.getElementById("methodAnd").checked) {
@@ -91,56 +79,49 @@ async function main() {
 
     document.getElementById("output").innerHTML = "Working on your request...";
 
-    for (let cate of [catName1, catName2]) {
-        if ([catName1, catName2].indexOf(cate) == 0) {
-            for (let item of cate) {
-                if (item.includes('%')) {
-                    await getJason("https://" + wiki + "/api.php?action=query&cmtitle=Category:" + item + "&list=categorymembers&cmlimit=500&origin=*&format=json", rips1);
-                }
-                else {
-                    await getJason("https://" + wiki + "/api.php?action=query&cmtitle=Category:" + encodeURIComponent(item) + "&list=categorymembers&cmlimit=500&origin=*&format=json", rips1);
-                }
-            }
-        }
-        else {
-            for (let item of cate) {
-                if (item.includes('%')) {
-                    await getJason("https://" + wiki + "/api.php?action=query&cmtitle=Category:" + item + "&list=categorymembers&cmlimit=500&origin=*&format=json", rips2_1);
-                }
-                else {
-                    await getJason("https://" + wiki + "/api.php?action=query&cmtitle=Category:" + encodeURIComponent(item) + "&list=categorymembers&cmlimit=500&origin=*&format=json", rips2_1);
-                }
-                if (method == "and") {
-                    if (cate.indexOf(item) == 0) {
-                        rips2 = rips2_1;
-                    }
-                    else {
-                        rips2 = intersection(rips2_1, rips2);
-                    }
-                }
-                else {
-                    rips2 = rips2.concat(rips2_1);
-                }
+    // Category 1
+    for (let item of catName1) {
+        if (item.includes('%')) await getJason(item, rips1);
+        else await getJason(encodeURIComponent(item), rips1);
+    }
+
+    // Category 2
+    if (catName2 == "") rips2 = rips1;
+    else {
+        for (let item of catName2) {
+            if (item.includes('%')) await getJason(item, rips2_1);
+            else await getJason(encodeURIComponent(item), rips2_1);
+
+            if (method == "and") {
+                if (catName2.indexOf(item) == 0) rips2 = rips2_1;
+                else rips2 = await intersection(rips2_1, rips2);
                 rips2_1 = [];
             }
-        }
-    }
-    for (let item of catNeg) {
-        if (item.includes('%')) {
-            await getJason("https://" + wiki + "/api.php?action=query&cmtitle=Category:" + item + "&list=categorymembers&cmlimit=500&origin=*&format=json", negRips);
-        }
-        else {
-            await getJason("https://" + wiki + "/api.php?action=query&cmtitle=Category:" + encodeURIComponent(item) + "&list=categorymembers&cmlimit=500&origin=*&format=json", negRips);
+            else rips2 = rips2_1
         }
     }
 
+    // Negative category
+    if (catNeg != "") {
+        for (let item of catNeg) {
+            if (item.includes('%')) await getJason(item, negRips);
+            else await getJason(encodeURIComponent(item), negRips);
+        }
+    }
+
+    // Get rid of duplicate items in rips1
     rips1_1 = new Set(rips1)
     rips1 = Array.from(rips1_1);
 
+    // Intersect rips1 and rips2 and put the result in rips1
     rips1 = await intersection(rips1, rips2);
 
-    rips1 = rips1.filter(x => !negRips.includes(x));
+    // Remove negative rips if there are any
+    if (catNeg != "") {
+        rips1 = rips1.filter(x => !negRips.includes(x));
+    }
 
+    // Apply name filter if there are any
     if (nameFilter != "") {
         for (let i = rips1.length - 1; i >= 0; i--) {
             if (!rips1[i].includes(nameFilter)) {
@@ -149,6 +130,7 @@ async function main() {
         }
     }
 
+    // Check boxes
     if (document.getElementById('linkButton').checked) {
         for (let item of rips1) {
             rips1[rips1.indexOf(item)] = "<a href=\"" + "https://" + wiki + "\\wiki\\" + encodeURIComponent(item).replace("#", "") + "\">" + item + "</a>";
